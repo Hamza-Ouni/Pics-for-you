27e5edd1c83971825f59703f04f3d5908526d639
hamza-ouni <hamza-ouni@outlook.com>
2021-03-23 01:03:38 +0100

    build a list of records

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,22 @@
 import React from "react";
 import unsplash from "../api/unsplash";
 import SearchBar from "./SearchBar";
+import ImageList from "./ImagesList";
 
 class App extends React.Component {
   state = { images: [] };
-   onSearchSubmit = async (term) => {
+  onSearchSubmit = async (term) => {
     // console.log(term);
     const response = await unsplash.get("/search/photos", {
       params: { query: term },
-      
     });
-    console.log("this", this);
     this.setState({ images: response.data.results });
-  }
+  };
   render() {
     return (
       <div className="ui container" style={{ marginTop: "10px" }}>
         <SearchBar onSubmit={this.onSearchSubmit} />
-        Found : {this.state.images.length} images
+        <ImageList images = {this.state.images}/>
       </div>
     );
   }
