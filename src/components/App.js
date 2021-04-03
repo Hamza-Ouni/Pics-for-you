4f1614c0bafc91309ed54af66d5135227d9822a4
hamza-ouni <hamza-ouni@outlook.com>
2021-04-03 18:46:41 +0100

    Styling images

--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,6 @@ import ImageList from "./ImagesList";
 class App extends React.Component {
   state = { images: [] };
   onSearchSubmit = async (term) => {
-    // console.log(term);
     const response = await unsplash.get("/search/photos", {
       params: { query: term },
     });
--- src/components/ImagesList.js
+++ src/components/ImagesList.js
@@ -1,10 +1,11 @@
 import React from "react";
+import "./ImageList.css";
 
 const ImageList = (props) => {
-  const images = props.images.map(({description, urls ,id}) => {
+  const images = props.images.map(({ description, urls, id }) => {
     return <img key={id} alt={description} src={urls.regular} />;
   });
-  return <div>{images}</div>;
+  return <div className="image-list">{images}</div>;
 };
 
 export default ImageList;
